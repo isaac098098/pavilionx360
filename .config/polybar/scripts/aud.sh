adcf4be063c7d1f6196db51c4583012962a48f59
isaac09809 <joseluis-jl-1@hotmail.com>
2025-03-05 23:57:10 -0600

    earphones battery polybar module

---
+++ .config/polybar/scripts/aud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $(upower --dump | grep -C 7 'BC:A0:80:2C:AA:18') ]]
+then
+    echo "$(upower --dump | grep -C 7 'BC:A0:80:2C:AA:18' | grep -o '..%')"
+    exit 0
+else
+    echo "AUD"
+    exit 1
+fi
